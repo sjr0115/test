529e7e7297a32896e4a5817856337508feca1805
jeongryeolseo <jeongryeolseo@218.38.137.27>
2020-08-04 21:28:41 +0900

    test commit 1

--- kb_test/src/kb_test/git_test.java
+++ kb_test/src/kb_test/git_test.java
@@ -3,7 +3,11 @@ package kb_test;
 public class git_test {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		for (int i = 0; i < 3; i++) {
+			System.out.println("number: " + i + "입니다.");
+		}
+		System.out.println("test1");
+		
 	}
 
 }
